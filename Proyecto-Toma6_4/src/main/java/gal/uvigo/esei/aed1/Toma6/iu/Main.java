135732bcc2ebd4c66bed9b8a61fdd2f13459f323
DeTiveNC <116792124+DeTiveNC@users.noreply.github.com>
2024-03-19 17:50:20 +0100

    💻 Proyecto AEDI - Toma6 inicial

--- Proyecto-Toma6_4/src/main/java/gal/uvigo/esei/aed1/Toma6/iu/Main.java
+++ Proyecto-Toma6_4/src/main/java/gal/uvigo/esei/aed1/Toma6/iu/Main.java
@@ -4,6 +4,10 @@ package gal.uvigo.esei.aed1.Toma6.iu;
 import gal.uvigo.esei.aed1.Toma6.core.Juego;
 
 public class Main {
+    /**
+     * Método principal de la aplicación
+     * @param args
+     */
     public static void main(String[] args) {              
         IU iu = new IU();
         Juego toma6 = new Juego(iu);
