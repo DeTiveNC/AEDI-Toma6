2b9bb7d6aa3ab75c3a80b2d48aebd50f774dd8b8
alicianose <114583435+alicianose@users.noreply.github.com>
2024-04-17 20:20:59 +0200

    Update Juego.java

--- Proyecto-Toma6_4/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Juego.java
+++ Proyecto-Toma6_4/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Juego.java
@@ -5,13 +5,16 @@
 package gal.uvigo.esei.aed1.Toma6.core;
 
 import gal.uvigo.esei.aed1.Toma6.iu.IU;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * This class represents the game Toma 6.
  */
 public class Juego {
+
     // The user interface for the game
     private final IU iu;
     // The players in the game
@@ -23,9 +26,10 @@ public class Juego {
 
     /**
      * Constructs a new game with the given user interface.
+     *
      * @param iu the user interface for the game
      */
-    public Juego(IU iu){
+    public Juego(IU iu) {
         this.iu = iu;
         this.jugadores = new LinkedList<>();
     }
@@ -33,7 +37,7 @@ public class Juego {
     /**
      * Starts the game.
      */
-    public void jugar(){
+    public void jugar() {
         crearBaraja();
         insertarJugadores();
         repartirCartas();
@@ -41,6 +45,7 @@ public class Juego {
         crearMesa();
         colocarCartasInicialesMesa();
         mostrarInformacionMesa();
+        jugando();
     }
 
     /**
@@ -55,7 +60,7 @@ public class Juego {
      */
     public void insertarJugadores() {
         Collection<String> nombres = iu.pedirNombresJugadores();
-        for (String nombre : nombres){
+        for (String nombre : nombres) {
             jugadores.add(new Jugador(nombre));
         }
     }
@@ -64,7 +69,7 @@ public class Juego {
      * Distributes 10 cards to each player.
      */
     public void repartirCartas() {
-        for (int i = 0; i < 10; i ++) {
+        for (int i = 0; i < 10; i++) {
             for (Jugador j : jugadores) {
                 j.anadirCarta(baraja.getCarta());
             }
@@ -82,7 +87,7 @@ public class Juego {
     /**
      * Creates a new table for the game.
      */
-    public void crearMesa(){
+    public void crearMesa() {
         System.out.println("Creando la mesa de juego");
         mesa = new Mesa();
     }
@@ -90,8 +95,8 @@ public class Juego {
     /**
      * Places the initial cards on the table.
      */
-    public void colocarCartasInicialesMesa(){
-        for (int i = 0; i < 4; i++){
+    public void colocarCartasInicialesMesa() {
+        for (int i = 0; i < 4; i++) {
             mesa.insertarCartas(baraja.getCarta());
         }
     }
@@ -99,7 +104,58 @@ public class Juego {
     /**
      * Displays information about the table in the game.
      */
-    public void mostrarInformacionMesa(){
+    public void mostrarInformacionMesa() {
         iu.mostrarMesa(mesa);
     }
+    
+    public void jugando(){
+        for (Jugador jugador : jugadores) {
+            while(!jugador.getMano().isEmpty()){
+                seleccionarCartaMano();
+            }
+        }
+
+    }
+
+    public void seleccionarCartaMano() {
+        List<Carta> cartasSeleccionadas = new ArrayList<>();
+
+        // Cada jugador selecciona una carta y la muestra
+        for (Jugador jugador : jugadores) {
+            int pos;
+            System.out.println("\nCartas de "+jugador.getNombre()+"\n"+jugador.getMano());
+            do {
+                pos = iu.leeNum("\nJugador " + jugador.getNombre() + ", introduce la posición de la carta a seleccionar: ");
+
+            } while (pos <= 0 || pos > jugador.getMano().size());
+
+            Carta cartaSeleccionada = jugador.getMano().get(pos - 1);
+            jugador.getMano().remove(pos-1);
+            System.out.println("Jugador " + jugador.getNombre() + " ha seleccionado la carta: " + cartaSeleccionada);
+            //Vamos a insertarla en orden
+            insertarEnOrden(cartasSeleccionadas, cartaSeleccionada);
+        }
+
+        // Coloca las cartas en la mesa en el orden determinado
+        for (Carta carta : cartasSeleccionadas) {
+            mesa.insertarCartas(carta);
+
+        }
+        System.out.println("Estado de la mesa después de colocar la carta: ");
+        System.out.println(mesa);
+
+    }
+
+    public void insertarEnOrden(List<Carta> lista, Carta carta) {
+        if (lista.isEmpty()) {
+        lista.add(carta);
+    } else {
+        int indice = 0;
+        while (indice < lista.size() && lista.get(indice).number() < carta.number()) {
+            indice++;
+        }
+        lista.add(indice, carta);
+    }
+    }
+
 }
